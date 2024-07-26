b561f165de1dc873b6c915c29e3c85d131dd2bcd
Pavi-1210 <pavithra@entrans.ai>
2024-07-26 11:06:20 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.getenv('HUGGINGFACEHUB_API_TOKEN')
+# os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.getenv('HUGGINGFACEHUB_API_TOKEN')
 os.environ['COHERE_API_KEY'] = os.getenv('COHERE_API_KEY')
 os.environ["GOOGLE_API_KEY"]=os.getenv('GOOGLE_API_KEY')
 os.environ["PINECONE_API_KEY"]=os.getenv('PINECONE_API_KEY')
